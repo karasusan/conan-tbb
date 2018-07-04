cb6f5f3116de3a1d451414a1bef47a4de7881bd7
kazuki matsumoto <karasusan@gmail.com>
2018-07-04 13:19:16 +0900

    fix build error

--- build.py
+++ build.py
@@ -5,9 +5,11 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="TBB:shared")
-
-    if platform.system() == "Windows":
-        for i in range(len(builder.items)):
-            builder[i].options["TBB:shared"] = True
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if platform.system() == "Windows" and not options["TBB:shared"]:
+            options["TBB:shared"] = True
+        filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
 
     builder.run()
