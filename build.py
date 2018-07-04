cad97389010dc7dc06e2691415243f8721aaa6d3
kazuki matsumoto <karasusan@gmail.com>
2018-07-04 13:03:10 +0900

    Test "shared" flag change to True on Windows

--- build.py
+++ build.py
@@ -5,9 +5,9 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="TBB:shared")
-    filtered_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        if platform.system() != "Windows" or options["TBB:shared"]:
-            filtered_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = filtered_builds
+
+    if platform.system() == "Windows":
+        for i in range(len(builder.items)):
+            builder[i].options["TBB:shared"] = True
+
     builder.run()
